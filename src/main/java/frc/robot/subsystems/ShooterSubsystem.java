d0164d0879429aa3812c0c6606889daae21ce4d2
neev <nevadex@gmail.com>
2026-02-04 19:57:36 -0500

    remove 2024(!) code from shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,16 +1,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.ColorSensorV3;
-
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.ReduceCANUsage;
 import frc.robot.util.ReduceCANUsage.Spark_Max.Usage;
@@ -22,13 +15,6 @@ public class ShooterSubsystem extends SubsystemBase {
     private final SparkMaxConfig shooterMotor_2_Config;
     private final SparkMax shooterMotor_3;
     private final SparkMaxConfig shooterMotor_3_Config;
-    private boolean flywheelState = false;
-    private boolean ignoreSensor = false;
-    private final SendableChooser<Boolean> ignoreSensorChooser = new SendableChooser<>();
-   // private final LEDSubsystem led;
-    private boolean noteDetected;
-    public boolean detecting = true;
-   // private final GenericEntry entry;
 
     public ShooterSubsystem() {
         //led = ledSubsystem;
@@ -40,26 +26,8 @@ public class ShooterSubsystem extends SubsystemBase {
         shooterMotor_3 = new SparkMax(53, MotorType.kBrushless);
         shooterMotor_3_Config = new SparkMaxConfig();
         configMotors();
-
-
-        ignoreSensorChooser.addOption("deactivate sensor", false);
-        ignoreSensorChooser.setDefaultOption("sensor is active", true);
-        Shuffleboard.getTab("config").add("ignore intake sensor", ignoreSensorChooser).withWidget(BuiltInWidgets.kSplitButtonChooser).withSize(2, 1);
-        ignoreSensorChooser.onChange((Boolean val) -> ignoreSensor = val);
-
-        //Shuffleboard.getTab("debug").addNumber("Intake Motor Current", intakeMotor::getOutputCurrent);
-        //entry = Shuffleboard.getTab("config").add("amp speed").getEntry();
-        /* 
-        Shuffleboard.getTab("debug").addNumber("NOTE DISTANCE", () -> {
-            noteDetected = prox > INTAKE_SENSOR_THRESHOLD;
-            return prox;
-        });
-        */
-        Shuffleboard.getTab("debug").addBoolean("DETECTED", () -> noteDetected);
-        Shuffleboard.getTab("debug").addBoolean("detecting?", () -> detecting);
     }
 
-    //configDriveMotor();
     private void configMotors() {
         //shooterMotor_1.restoreFactoryDefaults();
         ReduceCANUsage.Spark_Max.setCANSparkMaxBusUsage(shooterMotor_1, Usage.kAll, shooterMotor_1_Config);
@@ -77,14 +45,9 @@ public class ShooterSubsystem extends SubsystemBase {
         shooterMotor_3_Config.inverted(false);
         shooterMotor_3_Config.idleMode(IdleMode.kBrake);
         shooterMotor_3_Config.voltageCompensation(12.0);
-       
-        
     }
 
     public void shootMotor(boolean shoot, double speed) {
-        
-        flywheelState = shoot;
-
         if (shoot) {
             shooterMotor_1.set(speed);
             shooterMotor_2.set(1);
@@ -95,66 +58,10 @@ public class ShooterSubsystem extends SubsystemBase {
             shooterMotor_2.set(0);
             shooterMotor_3.set(0);
         }
+    }
 
-    }
-/* 
-    public void flywheelAmp(boolean shoot) {
-        flywheelState = shoot;
-        if (shoot) {
-//             shooterMotor_1.set(AMP_SPEED);
-            shooterMotor_1.set(1);
-            shooterMotor_2.set(1);
-            shooterMotor_3.set(1);
-        } else {
-            System.out.println("lol");
-            shooterMotor_1.set(0);
-            shooterMotor_2.set(0);
-            shooterMotor_3.set(0);
-        }
-    }
-*/
-/* 
-    public void receive(boolean shoot) {
-        if (shoot && !noteDetected) {
-            shooterMotor_1.set(-.5);
-        } else {
-            shooterMotor_1.set(0);
-        }
-    }
-*/
-    public void intake(boolean shoot, boolean reverse) {
-        if (ignoreSensor) {
-            if (shoot) {
-                
-            } else {
-               
-            }
-        } else {
-            var detected = noteDetected;
-            if (!detecting) {
-                detected = true;
-            }
-            if (flywheelState || reverse) {
-                detected = false;
-            }
-
-            if (shoot && !detected) {
-                
-            } else {
-                
-            }
-        }
-    }
+    public void intake(boolean shoot, boolean reverse) {}
 
     @Override
-    public void periodic() {
-        if (noteDetected && !flywheelState) {
-            detecting = false;
-          
-        } else if (flywheelState) {
-         
-        } else {
-         
-        }
-    }
+    public void periodic() {}
 }
