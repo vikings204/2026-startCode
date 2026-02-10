277405bb525043d5e5f86440b5fd58b89cc6c509
neev <nevadex@gmail.com>
2026-02-10 13:35:54 -0500

    reformat RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,13 +29,14 @@ public class RobotContainer {
     public final LEDSubsystem LED = new LEDSubsystem();
     public final IntakeSubsystem Intake = new IntakeSubsystem();
     public final ShooterSubsystem Shooter = new ShooterSubsystem();
-    public final ClimberSubsystem Climber = new ClimberSubsystem();
+    //public final ClimberSubsystem Climber = new ClimberSubsystem();
     public final PoseEstimationSubsystem PoseEstimation = new PoseEstimationSubsystem(Swerve::getYaw, Swerve::getPositions, Swerve::getSpeeds);
 
     Gamepad DRIVER = new Gamepad(Controller.DRIVER_PORT);
     Gamepad OPERATOR = new Gamepad(Controller.OPERATOR_PORT);
 
     private final SendableChooser<Command> autoModeChooser;
+
     public RobotContainer() {
         ControlModeChooser.onChange((ControlMode mode) -> {
             if (mode == ControlMode.SINGLE) {
@@ -57,7 +58,7 @@ public class RobotContainer {
                 PoseEstimation::getPose, // Robot pose supplier
                 PoseEstimation::setPose,
                 Swerve::getSpeeds,
-                (speeds,feedforwards) -> Swerve.driveRobotRelative(speeds),//byass need for PathFeedfowards with lamda, hacky solution idk if its good// Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+                (speeds, feedforwards) -> Swerve.driveRobotRelative(speeds),//byass need for PathFeedfowards with lamda, hacky solution idk if its good// Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
                 Constants.Auto.PATH_FOLLOWER_CONFIG, // The path follower configuration 
                 Constants.Auto.config, // The robot configuration
                 //() -> Robot.alliance == DriverStation.Alliance.Red,
@@ -70,32 +71,33 @@ public class RobotContainer {
                 },
                 Swerve // Reference to this subsystem to set requirements
         );
-        
+
         boolean isCompetition = false;
 
         autoModeChooser = AutoBuilder.buildAutoChooserWithOptionsModifier(
-            (stream) -> isCompetition
-            ? stream.filter(auto -> auto.getName().startsWith("comp"))
-            : stream
+                (stream) -> isCompetition
+                        ? stream.filter(auto -> auto.getName().startsWith("comp"))
+                        : stream
         );
-        SmartDashboard.putData("Auto Chooser", autoModeChooser);        
+        SmartDashboard.putData("Auto Chooser", autoModeChooser);
 
         CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand());
 
         NamedCommands.registerCommand("ZERO", new InstantCommand(() -> Intake.setPosition(Positions.ZERO), Intake));
-        NamedCommands.registerCommand("INTAKE", new InstantCommand(() -> Intake.IntakeAUTO(), Intake));
+        NamedCommands.registerCommand("INTAKE", new InstantCommand(Intake::IntakeAUTO, Intake));
         NamedCommands.registerCommand("Shoot", new InstantCommand(() -> Shooter.shootMotor(true, 1), Shooter));
         NamedCommands.registerCommand("IntakeMotorON", new InstantCommand(() -> Intake.IntakeMotor(true), Intake));
         NamedCommands.registerCommand("IntakeMotorOFF", new InstantCommand(() -> Intake.IntakeMotor(false), Intake));
 
         NamedCommands.registerCommand("Intake_Elevator", new InstantCommand(() -> Intake.setPosition(Positions.INTAKE), Intake));
         NamedCommands.registerCommand("zeroGyro", new InstantCommand(Swerve::zeroGyro, Swerve));
-     
+
         configureDefaultCommands();
-        configureButtonBindings();   
+        configureButtonBindings();
     }
+
     public Command getAutonomousCommand() {
-         return new PathPlannerAuto(AutoModeChooser.getSelected().pathplannerName);
+        return new PathPlannerAuto(AutoModeChooser.getSelected().pathplannerName);
     }
 
     private void configureDefaultCommands() {
@@ -143,38 +145,38 @@ public class RobotContainer {
         new JoystickButton(OPERATOR, 4)
                 .onTrue(new RunCommand(() -> Intake.setPosition(Positions.INTAKE), Intake));
 
-      //  new JoystickButton(DRIVER, 1).
-       //         whileTrue(Swerve.driveToPose());
+        //  new JoystickButton(DRIVER, 1).
+        //         whileTrue(Swerve.driveToPose());
 //        new JoystickButton(DRIVER, 1).whileTrue(new AlignCommand(false, Swerve));
 
-  new JoystickButton(DRIVER, 4)
-        //.whileTrue(StupidAlignLeft); // A is left
-         .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .25), Shooter))
-        .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
+        new JoystickButton(DRIVER, 4)
+                //.whileTrue(StupidAlignLeft); // A is left
+                .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .25), Shooter))
+                .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
         new JoystickButton(DRIVER, 1)
-        //.whileTrue(StupidAlignRight); // B is right
-         .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .5), Shooter))
-        .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
+                //.whileTrue(StupidAlignRight); // B is right
+                .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .5), Shooter))
+                .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
         new JoystickButton(DRIVER, 2)
-        //.whileTrue(ColorAlignLeft);
-         .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .75), Shooter))
-        .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
+                //.whileTrue(ColorAlignLeft);
+                .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, .75), Shooter))
+                .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
         new JoystickButton(DRIVER, 3)
-        //.whileTrue(ColorAlignRight);
-        .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, 1), Shooter))
-        .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
+                //.whileTrue(ColorAlignRight);
+                .whileTrue(new InstantCommand(() -> Shooter.shootMotor(true, 1), Shooter))
+                .onFalse(new InstantCommand(() -> Shooter.shootMotor(false, 0), Shooter));
     }
-    
+
     public void checkAnalogs() {
-        if (OPERATOR.getRightTriggerAxis() > .5) {
-           // CommandScheduler.getInstance().schedule(new InstantCommand(() -> System.out.println("Command scheduled!")));
-        }
+        /*if (OPERATOR.getRightTriggerAxis() > .5) {
+            CommandScheduler.getInstance().schedule(new InstantCommand(() -> System.out.println("Command scheduled!")));
+        }*/
 
-        if (OPERATOR.getLeftTriggerAxis() > .5) {
-          //  CommandScheduler.getInstance().schedule(new InstantCommand(() -> System.out.println("Command scheduled!")));
-        }
+        /*if (OPERATOR.getLeftTriggerAxis() > .5) {
+            CommandScheduler.getInstance().schedule(new InstantCommand(() -> System.out.println("Command scheduled!")));
+        }*/
 
-        if (OPERATOR.getRightY() < -.5) {
+        /*if (OPERATOR.getRightY() < -.5) {
             CommandScheduler.getInstance().schedule(new RunCommand(() -> Climber.ShootArm(true), Climber));
         } else {
             CommandScheduler.getInstance().schedule(new RunCommand(() -> Climber.ShootArm(false), Climber));
@@ -182,7 +184,7 @@ public class RobotContainer {
 
         if (OPERATOR.getRightY() > .5) {
             CommandScheduler.getInstance().schedule(new RunCommand(() -> Climber.NegativeShootArm(true), Climber));
-            }
-        }
+        }*/
     }
+}
 
